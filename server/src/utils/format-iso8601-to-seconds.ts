2126ed9ce6c79ef822fef4be5ae7e059783c7f20
Thierry Tajes dos Santos <thierrytajesdossantos@gmail.com>
2023-08-18 20:38:30 -0300

    feat(utils): add format ISO8601 to seconds

---
+++ server/src/utils/format-iso8601-to-seconds.ts
@@ -0,0 +1,16 @@
+export function formatISO8601ToSeconds(duration: string): number {
+  const match = duration.match(/^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/);
+  if (match === null) {
+    return 0;
+  }
+
+  const hours = match[1] !== undefined ? parseInt(match[1]) : 0;
+  const minutes = match[2] !== undefined ? parseInt(match[2]) : 0;
+  const seconds = match[3] !== undefined ? parseInt(match[3]) : 0;
+
+  const hoursInSeconds = hours * 60 * 60;
+  const minutesInSeconds = minutes * 60;
+
+  const totalSeconds = hoursInSeconds + minutesInSeconds + seconds;
+  return totalSeconds;
+}
