2a5ff1c7c9159912585e6ffd3494f5e23ebc23b5
Thierry Tajes dos Santos <thierrytajesdossantos@gmail.com>
2023-08-23 12:19:18 -0300

    feat(routes): add morgan to log

--- server/src/main/config/app.ts
+++ server/src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { setupRoutes } from "./routes"
 import cors from "cors"
+import morgan from "morgan"
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors({ origin: "*" }))
+app.use(morgan(":method :url :status - :response-time ms"))
 setupRoutes(app)
