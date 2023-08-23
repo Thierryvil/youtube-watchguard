3e230980d9e713920c66c57d7a7f17728840d424
Thierry Tajes dos Santos <thierrytajesdossantos@gmail.com>
2023-08-23 17:28:57 -0300

    feat(project): add video card component

---
+++ web/app/components/video-card.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image from 'next/image';
+import { Video } from '../entities';
+import { formatTime } from '../utils';
+
+const VideoCard = ({ title, thumbnail, duration }: Video) => {
+  return (
+    <div className="border border-gray-500 p-4 m-4 max-w-md">
+      <div className="relative">
+        <Image src={thumbnail} alt={title} width={800} height={450} />
+        <span className="absolute bottom-2 right-2 bg-black bg-opacity-70 text-white py-1 px-2 rounded">
+          {formatTime(duration)}
+        </span>
+      </div>
+      <h3 className="mt-2 text-lg font-semibold">{title}</h3>
+    </div>
+  );
+};
+
+export default VideoCard;
